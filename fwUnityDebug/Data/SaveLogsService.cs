48f240d1def958b72204444481877f198c109ab3
filipton <37213766+filipton@users.noreply.github.com>
2020-08-11 21:06:45 +0200

    Initial commit

---
+++ fwUnityDebug/Data/SaveLogsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fwUnityDebug.Data
+{
+	public class SaveLogsService
+	{
+		public static List<string> Logs = new List<string>();
+	}
+}
